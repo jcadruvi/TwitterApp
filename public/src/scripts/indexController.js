34eba5040e5a0e7f5ce36147cdeebe2238ec6f10
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-01 15:34:13 -0800

    Ignore a strict warning.

--- public/src/scripts/indexController.js
+++ public/src/scripts/indexController.js
@@ -1,10 +1,12 @@
 (function() {
    'use strict';
-
    angular.module("app")
           .controller("indexController", ["$scope", "indexService", indexController]);
 
    function indexController($scope, indexService) {
+      var vm = this; // jshint ignore:line
 
+      vm.service = indexService;
+      vm.service.init();
    }
 })();
