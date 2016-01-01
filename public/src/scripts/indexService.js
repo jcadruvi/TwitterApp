2fe69a4e73a2119f28317c3b8c87044a60d3f0c5
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-01 15:16:32 -0800

    Adding the index service.

--- public/src/scripts/indexService.js
+++ public/src/scripts/indexService.js
@@ -4,6 +4,21 @@
            .factory("indexService", ["$http", indexService]);
 
     function indexService($http){
+        var self = {};
 
+        self.loading = true;
+        self.tweets = [];
+
+        self.getTweets = function() {
+            self.loading = true;
+            $http.get('/api/twitter/timeline').then(function (result) {
+                if (result.status && result.status.code === 200) {
+                    self.tweets = result.data;
+                }
+                self.loading = false;
+            });
+        };
+
+        return self;
     }
 })();
