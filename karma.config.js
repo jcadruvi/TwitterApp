a3a5491b0055bd29f6b525ca4e86f119677bb06f
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-02 12:36:55 -0800

    Adding karma and jasmine testing.

---
+++ karma.config.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome'],
+    frameworks: ['jasmine', 'angular-filesort'],
+    files: [
+      'public/dist/bower/jquery/dist/jquery.js',
+      'public/dist/bower/angular/angular.js',
+      'public/dist/bower/angular-ui-router/release/angular-ui-router.js',
+      'public/dist/bower/angular-mocks/angular-mocks.js',
+      'public/src/scripts/**/*.js',
+      'public/dist/scripts/templates.js',
+      'tests/public/**/*.spec.js'
+    ],
+    angularFilesort: {
+      whitelist: [
+        'public/src/scripts/**/*.js'
+      ]
+    }
+  });
+};
---
+++ tests/public/src/scripts/twitterTextFilter.spec.js
@@ -0,0 +1,6 @@
+describe("twitterText", function() {
+   it("should return null when input is null", function() {
+        var test = true;
+        expect(test).toBe(true);
+   });
+});
