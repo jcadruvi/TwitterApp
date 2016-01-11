35cbb5e6e9f0037861f532ff84e8c3dd79ace182
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-10 20:30:52 -0800

    Replacing of image is working.

--- public/src/scripts/twitterTextFilter.js
+++ public/src/scripts/twitterTextFilter.js
@@ -36,24 +36,35 @@
                     var anchor,
                         duplicateUrl,
                         duplicateUrlIndices,
+                        image,
                         text = input.text;
-                    if (input.entities && input.entities.urls) {
-                        duplicateUrlIndices = findAllDuplicateUrls(input);
-                        text = removeDuplicateUrls(text, duplicateUrlIndices);
-                        for (var i = 0; i < input.entities.urls.length; i++) {
-                            if (input.entities.urls[i].url && input.entities.urls[i].display_url) {
-                                // Test for a duplicate URL already replaced.
-                                duplicateUrl = false;
-                                for (var j = 0; j < i; j++) {
-                                    if (input.entities.urls[i].url == input.entities.urls[j].url) {
-                                        duplicateUrl = true;
-                                        break;
+                    if (input.entities) {
+                        if (input.entities.urls) {
+                            duplicateUrlIndices = findAllDuplicateUrls(input);
+                            text = removeDuplicateUrls(text, duplicateUrlIndices);
+                            for (var i = 0; i < input.entities.urls.length; i++) {
+                                if (input.entities.urls[i].url && input.entities.urls[i].display_url) {
+                                    // Test for a duplicate URL already replaced.
+                                    duplicateUrl = false;
+                                    for (var j = 0; j < i; j++) {
+                                        if (input.entities.urls[i].url == input.entities.urls[j].url) {
+                                            duplicateUrl = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!duplicateUrl) {
+                                        anchor = '<a target="_blank" href="' + input.entities.urls[i].url + '">' +
+                                            input.entities.urls[i].display_url + '</a>';
+                                        text = text.replace(new RegExp(input.entities.urls[i].url, 'g'), anchor);
                                     }
                                 }
-                                if(!duplicateUrl) {
-                                    anchor = '<a target="_blank" href="' + input.entities.urls[i].url + '">' +
-                                        input.entities.urls[i].display_url + '</a>';
-                                    text = text.replace(new RegExp(input.entities.urls[i].url, 'g'), anchor);
+                            }
+                        }
+                        if (input.entities.media) {
+                            for (i = 0; i < input.entities.media.length; i++) {
+                                if (input.entities.media[i].url && input.entities.media[i].media_url) {
+                                    image = '<img src="' + input.entities.media[i].media_url + '"/>'
+                                    text = text.replace(new RegExp(input.entities.media[i].url, 'g'), image);
                                 }
                             }
                         }
--- tests/public/src/scripts/twitterTextFilter.spec.js
+++ tests/public/src/scripts/twitterTextFilter.spec.js
@@ -5,6 +5,10 @@ describe("twitterText", function() {
         return '<a target="_blank" href="' + url + '">' + displayUrl + '</a>';
     };
 
+    var createImageTag = function (url) {
+        return '<img src="' + url + '"/>'
+    };
+
     beforeEach(module("app"));
     beforeEach(inject(function(_$filter_){
         $filter = _$filter_;
@@ -74,5 +78,72 @@ describe("twitterText", function() {
         filterResult = $filter('twitterText')(twitterData);
         expect(filterResult).toBe("Machine Learning for indentify the author of an email "
             + anchorTag + " ");
+    });
+    it("should handle image URL's in a tweet", function() {
+        var twitterData = {
+            "text": "Machine Learning for indentify the author of an email https://t.co/xEVBHeutDn",
+            "entities": {
+                "urls": [
+                    {
+                        "url": "https://t.co/v6TAqtM90E",
+                        "expanded_url": "http://flip.it/Vw10S",
+                        "display_url": "flip.it/Vw10S",
+                        "indices": [
+                            54,
+                            77
+                        ]
+                    },
+                    {
+                        "url": "https://t.co/v6TAqtM90E",
+                        "expanded_url": "http://flip.it/Vw10S",
+                        "display_url": "flip.it/Vw10S",
+                        "indices": [
+                            78,
+                            101
+                        ]
+                    }],
+                "media": [
+                    {
+                        "id": 661381802146181100,
+                        "id_str": "661381802146181120",
+                        "indices": [
+                            102,
+                            125
+                        ],
+                        "media_url": "http://pbs.twimg.com/media/CS2zPrHUsAAFupG.jpg",
+                        "media_url_https": "https://pbs.twimg.com/media/CS2zPrHUsAAFupG.jpg",
+                        "url": "https://t.co/xEVBHeutDn",
+                        "display_url": "pic.twitter.com/xEVBHeutDn",
+                        "expanded_url": "http://twitter.com/joshcadruvi/status/661381802284597248/photo/1",
+                        "type": "photo",
+                        "sizes": {
+                            "small": {
+                                "w": 340,
+                                "h": 227,
+                                "resize": "fit"
+                            },
+                            "large": {
+                                "w": 499,
+                                "h": 334,
+                                "resize": "fit"
+                            },
+                            "thumb": {
+                                "w": 150,
+                                "h": 150,
+                                "resize": "crop"
+                            },
+                            "medium": {
+                                "w": 499,
+                                "h": 334,
+                                "resize": "fit"
+                            }
+                        }
+                    }]
+            }
+        },
+        imageTag = createImageTag(twitterData.entities.media[0].media_url),
+        filterResult = $filter('twitterText')(twitterData);
+        expect(filterResult).toBe("Machine Learning for indentify the author of an email "
+            + imageTag);
     });
 });
