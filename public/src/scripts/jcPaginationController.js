b9a5436d102a674f01cba3e30a460a53fc22d2f6
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-23 09:45:57 -0800

    Fixing tests.

--- public/src/scripts/jcPaginationController.js
+++ public/src/scripts/jcPaginationController.js
@@ -8,6 +8,7 @@
         vm.allowNext = false;
         vm.currentPage = 1;
         vm.numberOfPages = 0;
+        vm.startPage = 1;
 
         var calculateAllowPreviousAndNext = function () {
             if (vm.currentPage && vm.numberOfPages) {
@@ -16,27 +17,42 @@
             }
         };
 
+        var calculateStartPage = function () {
+            vm.startPage = vm.currentPage - 2;
+            if(vm.startPage < 1) {
+                vm.startPage = 1;
+            }
+
+            if (vm.startPage + 4 > vm.numberOfPages) {
+                vm.startPage = vm.numberOfPages - 4;
+            }
+        };
+
         $scope.$watch(['pageSize', 'total'], function(){
             if (vm.total && vm.pageSize) {
                 vm.numberOfPages = Math.ceil(vm.total / vm.pageSize);
             }
             calculateAllowPreviousAndNext();
+            calculateStartPage();
         });
 
         $scope.$watch('currentPage', function () {
-           calculateAllowPreviousAndNext();
+            calculateAllowPreviousAndNext();
+            calculateStartPage();
         });
 
         vm.doNext = function () {
             if (vm.currentPage < vm.numberOfPages) {
                 vm.currentPage++;
             }
+            calculateStartPage();
         };
 
         vm.doPrevious = function () {
             if (vm.currentPage > 1) {
                 vm.currentPage--;
             }
+            calculateStartPage();
         };
     }
 })();
