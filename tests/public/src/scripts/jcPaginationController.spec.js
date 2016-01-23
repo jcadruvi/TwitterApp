409b4b26d9310b00430a9a06c7bcf64b1355bbe0
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-01-23 09:53:15 -0800

    Refactor numberOfPages tests.

--- tests/public/src/scripts/jcPaginationController.spec.js
+++ tests/public/src/scripts/jcPaginationController.spec.js
@@ -11,26 +11,28 @@ describe("jcPaginationController", function () {
         $rootScope = _$rootScope_;
     }));
 
-    it('should calculate numberOfPages correctly', function() {
-        var vm, $scope = $rootScope.$new();
-        vm = $controller('jcPaginationController', { $scope: $scope});
+    describe('numberOfPages variable', function () {
+        it('should calculate numberOfPages correctly', function() {
+            var vm, $scope = $rootScope.$new();
+            vm = $controller('jcPaginationController', { $scope: $scope});
 
-        expect(vm.numberOfPages).toBe(0);
-        vm.total = 50;
-        vm.pageSize = 10;
-        $scope.$apply();
-        expect(vm.numberOfPages).toBe(5);
-    });
+            expect(vm.numberOfPages).toBe(0);
+            vm.total = 50;
+            vm.pageSize = 10;
+            $scope.$apply();
+            expect(vm.numberOfPages).toBe(5);
+        });
 
-    it('should round up numberOfPages to nearest int.', function() {
-        var vm, $scope = $rootScope.$new();
-        vm = $controller('jcPaginationController', { $scope: $scope});
+        it('should round up numberOfPages to nearest int.', function() {
+            var vm, $scope = $rootScope.$new();
+            vm = $controller('jcPaginationController', { $scope: $scope});
 
-        expect(vm.numberOfPages).toBe(0);
-        vm.total = 51;
-        vm.pageSize = 10;
-        $scope.$apply();
-        expect(vm.numberOfPages).toBe(6);
+            expect(vm.numberOfPages).toBe(0);
+            vm.total = 51;
+            vm.pageSize = 10;
+            $scope.$apply();
+            expect(vm.numberOfPages).toBe(6);
+        });
     });
 
     describe('allowPrevious variable', function() {
