ba4df3cea1664a488a328a3b1465c6260e9c8c64
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-12-15 18:21:31 -0800

    Adding some older configuration changes.

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
     twitter: {
-        consumerKey: "XXX",
-        consumerSecret: "XXX",
-        accessToken: "XXX",
-        accessTokenSecret: "XXX",
-        callBackUrl: "XXX"
+        consumerKey: "e7RrKbgojem9jXxh5JlTYmXpl",
+        consumerSecret: "VwhpWt1vO0FoaALD4M8XtG7Iyu81gZxpzQuj33wo0vYfjZGrxj",
+        accessToken: "1546955072-P1kP0A6spJqN9r0XL9SIgq8Pkak7oJKC0seBTMF",
+        accessTokenSecret: "8OuQeBQar4KqvZBUBwJPLN8YZ7l5a8cLFutyrUead1kwr",
+        callBackUrl: ""
     }
 };
